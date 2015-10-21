7508d0b4568dc7dc14849f52fe3cd241e7f8215a
Mateusz.Kasiuba <Mateusz.Kasiuba@jisc.ac.uk>
2015-10-21 12:41:36 +0100

    The newest commit

--- HelloWord/root/nested/example.py
+++ HelloWord/root/nested/example.py
@@ -3,13 +3,14 @@ Created on 21 Oct 2015
 
 @author: Mateusz.Kasiuba
 '''
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    return 'Hello Worldddsdsd!'
+    
 if __name__ == '__main__':
     app.run()
-    test failed
+print('important thing')
