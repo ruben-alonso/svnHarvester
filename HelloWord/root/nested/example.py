c83aa82aff58d2386228816cd06abe30f5cd48c2
Mateusz.Kasiuba <Mateusz.Kasiuba@jisc.ac.uk>
2015-10-21 11:41:10 +0100

    Next test useless commit

---
+++ HelloWord/root/nested/example.py
@@ -0,0 +1,15 @@
+'''
+Created on 21 Oct 2015
+
+@author: Mateusz.Kasiuba
+'''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+    test failed
