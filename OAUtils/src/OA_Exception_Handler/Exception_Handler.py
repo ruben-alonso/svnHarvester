ccfa1b4d0fe9896debefc13369ad6d2d36431522
Ruben.Alonso <Ruben.Alonso@jisc.ac.uk>
2015-11-04 15:35:25 +0000

    First OAUtils commit

---
+++ OAUtils/src/OA_Exception_Handler/Exception_Handler.py
@@ -0,0 +1,71 @@
+'''
+Created on 26 Oct 2015
+
+@author: Ruben.Alonso
+
+@Description: Module created to manage all different errors and exceptions.
+
+'''
+import OA_Logging_Handler.Logging_Handler as LH
+
+
+class Error(Exception):
+
+    """Base class for exceptions in this module."""
+    pass
+
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        message -- explanation of the error
+        error -- actual error information
+    """
+
+    def __init__(self, message="Incorrect input exception", error=""):
+        LH.fileLogger.error(message + error)
+        self.error = error
+        self.message = message
+
+
+class IncorrectFormatError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        message -- explanation of the error
+        error -- actual error information
+    """
+
+    def __init__(self, message="Incorrect format exception", error=""):
+        LH.fileLogger.error(message + error)
+        self.error = error
+        self.message = message
+
+
+class DBConnectionError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        message -- explanation of the error
+        error -- actual error information
+    """
+
+    def __init__(self, message="DB connection exception", error=""):
+        LH.fileLogger.error(message + error)
+        self.error = error
+        self.message = message
+
+
+class GenericError(Error):
+    """Exception raised for generic errors.
+
+    Attributes:
+        message -- explanation of the error
+        error -- actual error information
+    """
+
+    def __init__(self, message="Generic exception", error=""):
+        LH.fileLogger.error(message + error)
+        self.error = error
+        self.message = message
---
+++ OAUtils/src/config.py
@@ -0,0 +1,20 @@
+'''
+Created on 26 Oct 2015
+
+@author: Ruben.Alonso
+'''
+configMySQL = {
+               'user': 'my',
+               'password': 'sql',
+               'host': '127.0.0.1',
+               'database': 'articles',
+               'raise_on_warnings': True,
+               }
+
+configES = [{'host': '10.100.13.103', 'port': 9200}]
+
+queryPMC = "http://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?"
+
+queryPubMed = ""
+
+queryEPMC = "http://www.ebi.ac.uk/europepmc/webservices/rest/profile?query="
