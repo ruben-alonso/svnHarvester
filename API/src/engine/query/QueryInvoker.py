77fbd08b042b5ad25e8e2fdc0ea828a90d9a3eed
Karmele <karmele.hernaiz@jisc.ac.uk>
2015-11-16 16:01:58 +0000

    new src folder

---
+++ API/src/engine/query/QueryInvoker.py
@@ -0,0 +1,19 @@
+'''
+Created on 06 Nov 2015
+
+Engine invoker
+
+@author: Mateusz.Kasiuba
+'''
+from engine.query.QueryEngineMultiPage import H_QueryEngineMultiPage
+from utils.config import MULTI_PAGE
+
+
+class H_QueryInvoker():
+    
+    @staticmethod
+    def get_engine(engine_name, db, url, date_start = 0, date_end = 0):
+        if(engine_name == MULTI_PAGE):
+            return H_QueryEngineMultiPage(db, url, date_start, date_end)
+        
+        raise ValueError('Engine %s do not exists!'% engine_name)
